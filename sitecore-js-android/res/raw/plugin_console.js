fff26b54527009b6f7e588aa49526713b498077e
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2013-12-16 15:33:54 +0200

    Configured gradle to use sitecore sdk repository.

--- sitecore-js-android/res/raw/plugin_console.js
+++
@@ -1,8 +0,0 @@
-function Console() {
-    this.log = function (message) {
-        var plugin = new scmobile.utils.ScPlugin('console');
-        var params = {message: message};
-        plugin.exec('log', params);
-    }
-}
-scmobile.console = new Console();
--- sitecore-qr-reader/src/net/sitecore/android/qr/QrReaderActivity.java
+++
@@ -1,9 +0,0 @@
-package net.sitecore.android.qr;
-
-import com.google.zxing.client.android.CaptureActivity;
-
-public class QrReaderActivity extends CaptureActivity {
-
-    public static final String EXTRA_SCAN_RESULT = "SCAN_RESULT";
-
-}
