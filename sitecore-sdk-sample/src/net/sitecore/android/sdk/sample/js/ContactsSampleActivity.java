00bb7f41d965dd78f11163fa590c1d27ccff479c
Kirill Zotin <kzotin@gmail.com>
2014-03-18 15:26:08 +0200

    Fixed contacts js bug.

--- sitecore-sdk-sample/src/net/sitecore/android/sdk/sample/js/ContactsSampleActivity.java
+++ sitecore-sdk-sample/src/net/sitecore/android/sdk/sample/js/ContactsSampleActivity.java
@@ -3,6 +3,7 @@ package net.sitecore.android.sdk.sample.js;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import net.sitecore.android.sdk.web.LogUtils;
 import net.sitecore.android.sdk.sample.R;
 import net.sitecore.android.sdk.web.ScWebViewSupportFragment;
 
@@ -12,6 +13,7 @@ public class ContactsSampleActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        LogUtils.setLogEnabled(true);
         setContentView(R.layout.main_support_fragment);
         ScWebViewSupportFragment fragment = (ScWebViewSupportFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_scmobile);
         fragment.getWebView().loadUrl("file:///android_asset/www/contacts.html");
